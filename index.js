dac379b4db79957c9089da913df6074e701342dc
DhruvGahoi <dhruvgahoi11111@gmail.com>
2023-10-06 19:14:07 +0530

    some UI changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const submitBtn = document.getElementById('submitBtn');
 const header = document.getElementById('header');
 
-
 const validInput =(numFloors,numLifts)=>{
 if(numLifts > numFloors){
       alert("No of lifts cannot be greater than number of floors");
@@ -19,36 +18,50 @@ if(numLifts > numFloors){
 submitBtn.addEventListener('click',()=>{
   const numFloors = document.getElementById("numFloors").value;
   const numLifts = document.getElementById("numLifts").value;
-  console.log(numFloors)
-  console.log(numLifts)
-  console.log("Coming inside Function");
-  const numfloors = document.getElementById("numFloors");
-  console.log(numfloors)
-  console.log("clicked");
   const isValid = validInput(numFloors,numLifts);
-  console.log(isValid)
   if(isValid) {
-    const liftContainer = document.getElementById("liftContainer");
-    liftContainer.innerHTML = ""; 
-    
-    // Generate the lifts
-    console.log(numLifts)
-    for (let i = 1; i <= numLifts; i++) {
-    const lift = document.createElement("div");
-    lift.className = "lift";
-    lift.innerText = "Lift " + i;
-    liftContainer.appendChild(lift);
+
+    const container = document.getElementById("container");
+    container.innerHTML = ""; 
+
+
+  for (let floor = numFloors; floor >= 1; floor--) {
+    let floorContainer = document.createElement("div");
+    floorContainer.classList.add("floor-container");
+    floorContainer.innerText = "Floor " + floor;
+    floorContainer.setAttribute("id", `floor-${floor}`);
+    container.appendChild(floorContainer);
+
+    let floorButtonUp = document.createElement("button");
+    floorButtonUp.classList.add("button-up");
+    floorButtonUp.setAttribute("id", `buttonUp-${floor}`);
+
+    let floorButtonDown = document.createElement("button");
+    floorButtonDown.classList.add("button-down");
+    floorButtonDown.setAttribute("id", `buttonDown-${floor}`);
+  }
+
+
+  for (let lift = 1; lift <= numLifts; lift++) {
+    let liftContainer = document.createElement("div");
+    liftContainer.classList.add("lift-container");
+    liftContainer.className = "lift";
+    liftContainer.innerText = "Lift " + lift;
+    container.appendChild(liftContainer);
+
+    let leftDoor = document.createElement("div");
+    leftDoor.classList.add("left-door");
+
+    let rightDoor = document.createElement("div");
+    rightDoor.classList.add("right-door");
+
+    liftContainer.appendChild(leftDoor);
+    liftContainer.appendChild(rightDoor);
 
   }
 
-  // Generate the floors
-  for (let j = numFloors; j >= 1; j--) {
-    const floor = document.createElement("div");
-    floor.className = "floor";
-    floor.innerText = "Floor " + j;
-    liftContainer.appendChild(floor);
-  }
+ header.innerHTML=""
  }
- header.innerHTML="";
+
 })
 
